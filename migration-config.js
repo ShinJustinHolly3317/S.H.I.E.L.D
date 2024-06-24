b43f9d06da5de764f7e71a508630349081476afc
Justin Kao <justin.kao@choco.media>
2024-06-24 22:45:24 +0800

    chore: update migration.config, remove hard coded configs

--- migration-config.js
+++ migration-config.js
@@ -1,35 +1,34 @@
 module.exports = {
-	test: {
-		host: 'localhost',
-		database: 'video_ad',
-		username: 'aduser',
-		password: 'adtest',
-		port: 3306,
-		dialect: "mysql",
-		logging: true,
-		dialectOptions: {
-			multipleStatements: true
-		}
-	},
-	development: {
-		// host: 'localhost',
-		host: 'mysql-8.0_shield',
-		database: 'shield',
-		username: 'testuser',
-		password: 'test',
-		port: 3306,
-		dialect: "mysql",
-		logging: true,
-		dialectOptions: {
-			multipleStatements: true
-		}
-	},
-	production: {
-		username: process.env.DB_AD_WRITE_USERNAME,
-		password: process.env.DB_AD_WRITE_PASSWORD,
-		database: process.env.DB_AD_WRITE_DATABASE,
-		host: process.env.DB_AD_WRITE_ADDRESS,
-		dialect: "mysql",
-		logging: true
-	},
+  test: {
+    host: process.env.DB_ADDRESS,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: 3306,
+    dialect: 'mysql',
+    logging: true,
+    dialectOptions: {
+      multipleStatements: true,
+    },
+  },
+  development: {
+    host: process.env.DB_ADDRESS,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: 3306,
+    dialect: 'mysql',
+    logging: true,
+    dialectOptions: {
+      multipleStatements: true,
+    },
+  },
+  production: {
+    username: process.env.DB_AD_WRITE_USERNAME,
+    password: process.env.DB_AD_WRITE_PASSWORD,
+    database: process.env.DB_AD_WRITE_DATABASE,
+    host: process.env.DB_AD_WRITE_ADDRESS,
+    dialect: 'mysql',
+    logging: true,
+  },
 };
